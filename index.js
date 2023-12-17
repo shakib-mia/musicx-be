9e331844cc06b7ec438fddae03938bab4af4f984
Md. Shakib Mia <smdshakibmia2001@gmail.com>
2023-12-17 17:57:36 +0600

    getting revenue isrcs

--- index.js
+++ index.js
@@ -8,12 +8,20 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const verifyJWT = require("./verifyJWT");
 const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
 
-// console.log(uri);
 app.use(express());
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
+
+const transporter = nodemailer.createTransport({
+  service: "Outlook", // e.g., 'Gmail', 'SMTP'
+  auth: {
+    user: process.env.emailAddress,
+    pass: process.env.emailPass,
+  },
+});
 
 const port = process.env.port || 5000;
 
@@ -55,6 +63,10 @@ async function run() {
     const usersCollection = await client
       .db("forevision-digital")
       .collection("user-credentials-db");
+
+    const userDetails = await client
+      .db("forevision-digital")
+      .collection("user-details");
 
     /**
      *
@@ -106,11 +118,10 @@ async function run() {
       const isrc = await isrcCursor.toArray();
       const revenueCursor = await revenueCollections.find({});
       const revenues = await revenueCursor.toArray();
-      // console.log(users);
 
       const topContributor = users.reduce(
         (max, obj) =>
-          obj.isrc.split(",").length > max.isrc.split(",").length ? obj : max,
+          obj.isrc?.split(",").length > max.isrc?.split(",").length ? obj : max,
         users[0]
       );
 
@@ -118,8 +129,6 @@ async function run() {
         (max, obj) => (obj[" Royalty "] > max[" Royalty "] ? obj : max),
         revenues[0]
       );
-
-      // console.log(topSong);
 
       res.send({
         usersCount: users.length,
@@ -155,7 +164,6 @@ async function run() {
       const uploadCursor = await revenueCollections.insertMany(data);
 
       res.send(uploadCursor);
-      // console.log(data);
     });
 
     app.get("/revenue", verifyJWT, async (req, res) => {
@@ -164,6 +172,34 @@ async function run() {
       const revenues = await revenueCursor.toArray();
 
       res.send(revenues);
+    });
+
+    app.get("/user-revenue", verifyJWT, async (req, res) => {
+      const { email } = jwt.decode(req.headers.token);
+      // console.log(email);
+      const clientsCursor = await clientsCollection.findOne({
+        user_email: email,
+      });
+
+      // const
+
+      const isrcs = [];
+
+      clientsCursor.content_isrc
+        .split(",")
+        .map((item) => isrcs.push(item.trim()));
+
+      // isrcs.map((isrc) => {
+      //   const revenueCursor = revenueCollections.findOne({ isrc });
+      //   console.log(revenueCursor);
+      // });
+      const revenues = [];
+      for (const isrc of isrcs) {
+        const revenueCursor = await revenueCollections.findOne({ isrc });
+        revenueCursor !== null && revenues.push(revenueCursor);
+      }
+
+      console.log(revenues.length);
     });
 
     app.delete(
@@ -177,7 +213,6 @@ async function run() {
         const revenues = await revenueCursor.toArray();
         revenues.filter((item) => item.date.split("-")[1] === req.body.date);
 
-        // console.log(revenues);
         await revenues.map((item) => {
           const deleteCursor = revenueCollections.deleteOne({
             _id: new ObjectId(item._id),
@@ -189,20 +224,139 @@ async function run() {
     app.post("/user-login", async (req, res) => {
       const { email, password } = req.body;
       const userCursor = await usersCollection.findOne({ user_email: email });
-      // console.log(userCursor);
-      if (userCursor !== null) {
-        if (userCursor.password === password) {
-          // res.send({ message: "success" });
-          const token = jwt.sign({ email }, process.env.access_token_secret, {
-            expiresIn: "1h",
-          });
 
-          res.send({ token });
-        } else {
-          res.send({ message: "incorrect password" });
-        }
+      if (userCursor !== null) {
+        bcrypt.compare(password, userCursor.user_password, (err, result) => {
+          if (result) {
+            // res.send({ message: "success" });
+            const token = jwt.sign({ email }, process.env.access_token_secret, {
+              expiresIn: "1h",
+            });
+
+            res.send({ token });
+          } else {
+            res.status(401).send({ message: "incorrect password" });
+          }
+        });
       } else {
-        res.send({ message: "no user found" });
+        res.status(401).send({ message: "no user found" });
+      }
+    });
+
+    app.post("/reset-password", async (req, res) => {
+      const { user_email } = req.body;
+      const usersCursor = await usersCollection.findOne({ user_email });
+
+      if (usersCursor.password) {
+        var message = {
+          from: "abdullahalsamad@outlook.com",
+          to: user_email,
+          subject: "Reset Password",
+          // text: "Plaintext version of the message",
+          html: `<div>
+          Your Password new is 
+          <h1>${usersCursor.password}</h1>
+          </div>`,
+        };
+
+        transporter.sendMail(message, (error, info) => {
+          if (error) {
+            console.error(error);
+            res.status(500).send("Error sending email");
+          } else {
+            res.send("Email sent successfully");
+          }
+        });
+      } else {
+        function generatePassword() {
+          var length = 8,
+            charset =
+              "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+            retVal = "";
+          for (var i = 0, n = charset.length; i < length; ++i) {
+            retVal += charset.charAt(Math.floor(Math.random() * n));
+          }
+          return retVal;
+        }
+
+        const newPassword = generatePassword();
+
+        var message = {
+          from: "abdullahalsamad@outlook.com",
+          to: user_email,
+          subject: "Reset Password",
+          // text: "Plaintext version of the message",
+          html: `<div>
+          Your Password new is 
+          <h1>${newPassword}</h1>
+          </div>`,
+        };
+
+        transporter.sendMail(message, (error, info) => {
+          if (error) {
+            console.error(error);
+            res.status(500).send("Error sending email");
+          } else {
+            res.send("Email sent successfully");
+          }
+        });
+
+        const user = await usersCollection.findOne({ user_email });
+
+        const updateCursor = await usersCollection.updateOne(
+          { user_email },
+          { $set: { ...user, password: newPassword } },
+          { $upsert: true }
+        );
+
+        res.send({ updateCursor });
+      }
+    });
+
+    app.get("/getUserData", async (req, res) => {
+      const { token } = req.headers;
+      if (jwt.decode(token) !== null) {
+        const { email } = jwt.decode(token);
+
+        const data = await userDetails.findOne({ user_email: email });
+        // console.log(data);
+        res.send({ data });
+      }
+    });
+
+    app.post("/user-signup", async (req, res) => {
+      const reqBody = req.body;
+      const userExist = await usersCollection.findOne({
+        email: reqBody.email,
+      });
+
+      // if user doesn't exist
+      if (userExist === null) {
+        bcrypt.hash(reqBody.password, 10, async function (err, hash) {
+          // Store hash in your password DB.
+          if (hash.length) {
+            const user = {
+              user_email: reqBody.email,
+              user_password: hash,
+            };
+
+            const registerCursor = await usersCollection.insertOne(user);
+            res.send(registerCursor);
+          }
+        });
+      } else {
+        // if user exists
+        res.send("user already exist");
+      }
+    });
+
+    app.post("/post-user-details", async (req, res) => {
+      const { user_email } = req.body;
+      const foundUserDetails = await userDetails.findOne({ user_email });
+      if (foundUserDetails === null) {
+        const userDetailsCursor = await userDetails.insertOne(req.body);
+
+        res.send(userDetailsCursor);
       }
     });
   } finally {
