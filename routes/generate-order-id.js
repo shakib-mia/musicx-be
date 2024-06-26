c879d28a2f70d4accc79743cc4998f5a1d35d6f3
Md. Shakib Mia <smdshakibmia2001@gmail.com>
2024-06-26 16:49:50 +0600

    triller platform image added

--- routes/generate-order-id.js
+++ routes/generate-order-id.js
@@ -1,19 +1,44 @@
 const express = require("express");
 const getCollections = require("../constants");
 
-function generateOrderId() {
-  return Math.floor(10000 + Math.random() * 90000).toString();
+async function getNextOrderId() {
+  const { songs } = await getCollections();
+
+  // Find the maximum existing orderId and extract the numeric part
+  const latestSong = await songs
+    .find({ orderId: { $regex: /^FVDO\d{5}$/ } })
+    .sort({ orderId: -1 })
+    .limit(1)
+    .toArray();
+  const maxOrderId =
+    latestSong.length > 0 ? parseInt(latestSong[0].orderId.slice(4), 10) : 0;
+
+  // Increment the orderId
+  const nextOrderId = maxOrderId + 1;
+
+  // Pad the orderId to ensure it is 5 digits long and add the prefix
+  const paddedOrderId = nextOrderId.toString().padStart(5, "0");
+
+  return `FVDO${paddedOrderId}`;
 }
+
 async function generateUniqueOrderId() {
+  const { songs } = await getCollections();
+
   let orderId;
   let orderExists = true;
 
-  const { songs } = await getCollections();
-
   while (orderExists) {
-    orderId = generateOrderId();
+    // Generate the next orderId
+    orderId = await getNextOrderId();
     orderExists = await songs.findOne({ orderId });
+    if (orderExists) {
+      console.log(`Order ID ${orderId} already exists. Generating a new one.`);
+    }
   }
+
+  // Store the orderId in songs collection
+  // await songs.insertOne({ orderId });
 
   return orderId;
 }
@@ -21,9 +46,13 @@ async function generateUniqueOrderId() {
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  const uniqueOrderId = await generateUniqueOrderId();
-
-  res.send(uniqueOrderId);
+  try {
+    const uniqueOrderId = await generateUniqueOrderId();
+    res.send({ orderId: uniqueOrderId });
+  } catch (error) {
+    console.error("Error generating unique order ID:", error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 module.exports = router;
