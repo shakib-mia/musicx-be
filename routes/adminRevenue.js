c22a30ecf5e03fa385bd27ae4607100428c8524a
Md. Shakib Mia <smdshakibmia2001@gmail.com>
2024-06-26 13:23:30 +0600

    unnecessary codes are removed

--- routes/adminRevenue.js
+++ routes/adminRevenue.js
@@ -1,44 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const getCollections = require("../constants");
-const verifyJWT = require("../verifyJWT");
-const jwt = require("jsonwebtoken");
-
-// router.get("/",  async (req, res) => {
-//   //   console.log("object");
-//   const { clientsCollection } = await getCollections();
-//   const { email } = jwt.decode(req.headers.token);
-
-//   const clientsCursor = await clientsCollection.findOne({
-//     emailId: email,
-//   });
-
-//   // const
-
-//   const isrcs = [];
-
-//   if (clientsCollection !== null) {
-//     if (clientsCursor !== null) {
-//       clientsCursor.isrc.split(",").map((item) => isrcs.push(item.trim()));
-//       res.send(isrcs);
-//     } else {
-//       res.send({ message: "No isrc found in clientsCursor" });
-//     }
-//   } else {
-//     res.send({ message: "clientsCollection is null" });
-//   }
-
-//   // isrcs.map((isrc) => {
-//   //   const revenueCursor = revenueCollections.findOne({ isrc });
-//   // });
-//   // for (const isrc of isrcs) {
-//   //   const revenueCursor = await revenueCollections.find({ isrc });
-//   //   const allRevenues = await revenueCursor.toArray();
-
-//   //   // res.send(allRevenues);
-//   //   // revenueCursor !== null && revenues.push(revenueCursor);
-//   // }
-// });
 
 router.get("/:isrc", async (req, res) => {
   const { revenueCollections } = await getCollections();
